9c309cb791dbcc31731d9e567e7fdca02d9c8936
Carlos Loura <carlos.loura@getir.com>
2022-11-03 10:23:08 +0100

    tests

--- components/menu/menu.jsx
+++ components/menu/menu.jsx
@@ -11,36 +11,16 @@ export default function Menu() {
       <h1 className={styles.title}>Soraia & João</h1>
       <div className={styles.navigationContainer}>
         <div className={styles.line} />
-        <a
-          href="/home"
-          className={`${styles.menuItem} ${
-            asPath === '/home' ? styles.activeMenu : styles.nonActiveMenu
-          }`}
-        >
+        <a href="/home" className={`${styles.menuItem} ${asPath === '/home' ? styles.activeMenu : styles.nonActiveMenu}`}>
           Início
         </a>
-        <a
-          href="/invite"
-          className={`${styles.menuItem} ${
-            asPath === '/invite' ? styles.activeMenu : styles.nonActiveMenu
-          }`}
-        >
+        <a href="/invite" className={`${styles.menuItem} ${asPath === '/invite' ? styles.activeMenu : styles.nonActiveMenu}`}>
           Convite
         </a>
-        <a
-          href="/stay"
-          className={`${styles.menuItem} ${
-            asPath === '/stay' ? styles.activeMenu : styles.nonActiveMenu
-          }`}
-        >
+        <a href="/stay" className={`${styles.menuItem} ${asPath === '/stay' ? styles.activeMenu : styles.nonActiveMenu}`}>
           Onde Ficar
         </a>
-        <a
-          href="/rspv"
-          className={`${styles.menuItem} ${
-            asPath === '/rspv' ? styles.activeMenu : styles.nonActiveMenu
-          }`}
-        >
+        <a href="/rspv" className={`${styles.menuItem} ${asPath === '/rspv' ? styles.activeMenu : styles.nonActiveMenu}`}>
           RSPV
         </a>
         <div className={styles.line} />
