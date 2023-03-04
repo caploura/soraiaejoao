35e78f385a6767ec34f9388348254483939eb82a
Carlos Loura <carlos.loura@getir.com>
2023-03-04 19:02:46 +0100

    Commit

--- pages/stay/index.jsx
+++ pages/stay/index.jsx
@@ -19,8 +19,8 @@ export default function StayPage() {
             target="blank"
             href="https://www.booking.com/hotel/pt/cambeiros-guest-house.en-gb.html?aid=356980&label=gog235jc-1DCAsouwFCFWNhbWJlaXJvcy1ndWVzdC1ob3VzZUgzWANoqQGIAQGYAQm4AQfIAQzYAQPoAQGIAgGoAgO4Atr_jaAGwAIB0gIkMTI2Y2Q5M2EtMmRjYS00N2MyLTgwOTYtM2JhMGZhNWQzOTc22AIE4AIB&sid=2f8a35c7c9845d5f18af8aef4ac3dc1a&dist=0&keep_landing=1&sb_price_type=total&type=total&checkin=2023-05-19&checkout=2023-05-20&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0"
           >
-            <img className={styles.hotelImage} src="/224705802.jpg" alt="quarto" />
             <p className={styles.hotelText}>Cambeiros Guesthouse</p>
+            <img className={styles.hotelImage} src="/224705802.jpg" alt="quarto" />
           </a>
 
           <div className={styles.multiIconsContainer}>
